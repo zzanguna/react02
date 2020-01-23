741d0928a4114dfdd71934c60080c33ec2dcfa72
zzanguna <60169666+zzanguna@users.noreply.github.com>
2020-01-23 15:27:08 +0900

    웹팩 설정

---
+++ lecture/WordRelay.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const { Component } = React;
+//파일을 쪼개는 경우 필요 3
+//필요한 라이브러리를 가져오는 코드
+
+class WordRelay extends Component{
+
+    render(){
+
+    }
+}
+
+module.exports = WordRelay;
+//파일을 쪼개는 경우 필요 2
+//다음 파일에도 사용할 수 있도록 하는 코드
