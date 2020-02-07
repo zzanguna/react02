395e542d4b3f0d19722e0aafd59105dd471bed89
zzanguna <60169666+zzanguna@users.noreply.github.com>
2020-02-07 19:51:58 +0900

    Class 에서 props를 변경할 경우

--- numberbaseball/Try.jsx
+++ numberbaseball/Try.jsx
@@ -1,7 +1,29 @@
 import React, { PureComponent, memo, useState} from "react";
 
-/*class Try extends PureComponent{
+class Try extends PureComponent{
+    constructor(props){
+        super(props);
+        const filtered = this.props.filter(() => {
 
+        });
+        this.state = {
+            result : this.props.result,
+            try : filtered
+        }
+        //constructor는 세심한 동작이 필요할때 주로 사용.
+    }
+/*
+    state = {
+        result : this.props.result
+    }*/
+
+    onClick = () => {
+        this.setState(
+            {
+                result : 1
+            }
+        )
+    }
     render() {
         const { tryInfo } = this.props;
         return(
@@ -11,8 +33,8 @@ import React, { PureComponent, memo, useState} from "react";
             </li>
         );
     }
-}*/
-
+}
+/*
 const Try  = memo(({tryInfo})=> {
     //props를 변경할 경우
     //Try의 state로 넣어서 변경해야한다.
@@ -28,7 +50,7 @@ const Try  = memo(({tryInfo})=> {
             <div>{tryInfo.result}</div>
         </li>
     )
-})
+})*/
 
 
 export default Try;
