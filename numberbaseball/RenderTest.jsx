fb1c4470dd41f24adbc9c316c9ca97c8d2b89981
zzanguna <60169666+zzanguna@users.noreply.github.com>
2020-02-07 18:40:16 +0900

    PureComponent로 사용하기

--- numberbaseball/RenderTest.jsx
+++ numberbaseball/RenderTest.jsx
@@ -1,12 +1,20 @@
-import React, {Component} from "react";
+import React, {PureComponent} from "react";
 
-class RenderTest extends Component{
+class RenderTest extends PureComponent{
 
     state = {
-
+        counter: 0,
+        string: "hello",
+        number:1,
+        boolean:true,
+        object:{},
+        array:[]
     }
 
+
+
     //어떤 경우에 렌더링을 해줘야할지 정의
+/*
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         if(this.state.counter !== nextState.counter){
             return true;
@@ -14,9 +22,15 @@ class RenderTest extends Component{
         //예전 state와 현재 state가 다른경우 렌더링
         return false;
     }
+*/
+        //PureComponent 시 shouldComponentUpdate를 자동으로 해주는 Component
+        //하지만 array와 객체는 어려워 한다.
 
     onClick = () => {
-      this.setState({});
+        //불변성을 고려해서 만들어줘야한다.
+      this.setState({
+          array : [...this.state.array, 5]
+      });
     };
 
     render() {
