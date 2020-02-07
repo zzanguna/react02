fa07477f06b494f3aa75f42f8bc2e410b1d953a7
zzanguna <60169666+zzanguna@users.noreply.github.com>
2020-02-07 18:48:30 +0900

    Try Pure Component 적용

--- numberbaseball/Try.jsx
+++ numberbaseball/Try.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { PureComponent} from "react";
 
-const Try = ({ tryInfo }) => {
+class Try extends PureComponent{
+
+    render() {
+        const { tryInfo } = this.props;
+        return(
+            <li>
+                <div>{tryInfo.try}</div>
+                <div>{tryInfo.result}</div>
+            </li>
+        );
+    }
+}
+
+/*const Try = ({ tryInfo }) => {
         return (
             <li>
                 <div>{tryInfo.try}</div>
                 <div>{tryInfo.result}</div>
             </li>
         )
-}
+}*/
+
 
 export default Try;
